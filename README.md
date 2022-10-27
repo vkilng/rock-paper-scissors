6747d48acdc7867772c3432882a1ee584df9bc96
unknown <vinithkaradi747@gmail.com>
2022-10-27 20:17:13 +0530

    README corrections and name fixes

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # rock-paper-scissors
+
+- [Live Demo](https://vkilng.github.io/rock-paper-scissors/)
+
 - A simple implementation of grade-school classic “rock paper scissors”.
+
 - A project as part of an endeavour to learn web development at [The Odin Project](https://www.theodinproject.com/)
--- script.js
+++ script.js
@@ -1,42 +1,41 @@
+let score = {player:0, computer:0};
 let hand = ['rock','paper','scissors'];
+
 function getComputerChoice() {
-    let res = 0;
-    res = Math.floor(Math.random() * 3);
-    return hand[res];
+    let result = 0;
+    result = Math.floor(Math.random() * 3);
+    return hand[result];
 }
 
 function playRound(playerSelection,computerSelection) {
-    if(playerSelection == computerSelection) {
+    if(playerSelection === computerSelection) {
         return 'DRAW';
-    }else if(playerSelection=='rock' && computerSelection=='scissors'){
+    }else if(playerSelection === 'rock' && computerSelection === 'scissors'){
         return true;
-    }else if(playerSelection=='paper' && computerSelection=='rock'){
+    }else if(playerSelection === 'paper' && computerSelection === 'rock'){
         return true;
-    }else if(playerSelection=='scissors' && computerSelection=='paper') {
+    }else if(playerSelection === 'scissors' && computerSelection === 'paper') {
         return true; 
     }else {
         return false;
     };
 }
 
-let score = {player:0,computer:0};
-
 function game(playerSelection) {
     let computerSelection = getComputerChoice();
     console.log(computerSelection);
     let compelem = document.getElementById('computer');
-    if(computerSelection == 'rock'){
+    if(computerSelection  === 'rock'){
         compelem.style.backgroundImage = "url(resources/hand-rock.png)";
-    } else if(computerSelection == 'paper'){
+    } else if(computerSelection  === 'paper'){
         compelem.style.backgroundImage = "url(resources/hand-paper.png)";
-    } else if(computerSelection == 'scissors'){
+    } else if(computerSelection  === 'scissors'){
         compelem.style.backgroundImage = "url(resources/hand-scissors.png)";
     };
-    //const to = setTimeout(()=>{compelem.style.backgroundImage = null;},5000);
-    let res = playRound(playerSelection,computerSelection);
-    if(typeof res == "boolean") {
-        if(res) {
-            score.player=score.player+1;
+    let result = playRound(playerSelection,computerSelection);
+    if(typeof result  === "boolean") {
+        if(result) {
+            score.player = score.player+1;
             document.getElementById('playerscore').innerText = score.player;
         } else {
             score.computer=score.computer+1;
@@ -44,15 +43,15 @@ function game(playerSelection) {
         };
     };
     let final = document.getElementById('final');
-    if(score.player == 5){
+    if(score.player === 5){
         final.style.color = 'green';
         final.innerText = 'You Won';
         document.getElementById('player').style.pointerEvents = 'none';
         to = setTimeout(()=>{window.location.reload()},3000);
-    } else if(score.computer == 5){
+    } else if(score.computer === 5){
         final.style.color = 'red';
         final.innerText = 'You Lost';
         document.getElementById('player').style.pointerEvents = 'none';
-        to = setTimeout(()=>{window.location.reload()},3000);
+        to = setTimeout(() => {window.location.reload()},3000);
     }
 }
